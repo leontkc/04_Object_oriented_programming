24f8ae828fa3b4ed04a13abfdaccb992df643f3b
Leon Chiu <leontkchiu@gmail.com>
2016-06-02 17:36:16 -0400

    finished paperboy

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -1,49 +1,46 @@
 class Paperboy
 
-  attr_accessor :name, :side
+  attr_accessor :name, :side, :quota
 
-  def initialize(name, side)
+  def initialize(name, side, quota = 50)
     @name = name
     @side = side
+    @quota = quota
   end
 
   def experience
-    @deliveries.to_i
-  end
-
-  def quota
-    50 + experience / 2
+    0
   end
 
   def deliver(start_address, end_address)
-    @deliveries = (end_address - start_address) / 2
-    @money = @deliveries * 0.25
+    if end_address.even? == start_address.even?
+      @deliveries = ((end_address - start_address) / 2) + 1
+    elsif end_address.odd? == start_address.even?
+      @deliveries = ((end_address - start_address) / 2) + 1
+    end
+  end
+
+  def earnings
+    if @deliveries > quota
+     (@deliveries - quota) * 0.5 + quota * 0.25
+   else @deliveries * 0.25 - 2
+   end
   end
 
   def report
-    "I'm #{@name}, I've delivered #{@deliveries} papers and I've earned $#{@money} so far"
+    "I'm #{@name}, I've delivered #{@deliveries} papers and I've earned $#{earnings} so far"
   end
 
 end
 
-puts "What is the name of the paperboy?"
-name = gets.chomp.to_s
-side = ""
-  until ['odd', 'even'].include?(side)
-    puts "What side is he delivering on?"
-    side = gets.chomp.to_s
-  end
 
-
-
-  # if @side != 'even'
-  # else @side != 'odd'
-  # end
-  leon = Paperboy.new(name,side)
-  puts leon.inspect
-#puts 'What is the starting house number?'
-#@start_address = gets.chomp.to_i
-#   if @start_address.is_not_an_i
-
-#experience thing
-#restricting values in parameters
+#code to put inputs and initialize it as an object
+# puts "What is the name of the paperboy?"
+# name = gets.chomp.to_s
+# side = ""
+#   until ['odd', 'even'].include?(side)
+#     puts "What side is he delivering on?"
+#     side = gets.chomp.to_s
+#   end
+#  leon = Paperboy.new(name,side)
+#  puts leon.inspect
