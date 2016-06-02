652118ad6ddc1104c984d5380b7159ce0412f026
Leon Chiu <leontkchiu@gmail.com>
2016-06-02 15:19:01 -0400

    step 4

--- cat/cat.rb
+++ cat/cat.rb
@@ -1,8 +1,21 @@
 class Cat
+  attr_accessor :name, :preferred_food, :meal_time
+
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
   end
 
+  def eats_at
+    if @meal_time < 11
+      "#{@meal_time} AM"
+    else
+      "#{@meal_time - 12} PM"
+    end
+  end
+
 end
+
+leon = Cat.new('leon','steak',13)
+melissa = Cat.new('Melmo','sushi',12)
