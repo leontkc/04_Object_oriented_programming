9c14f5e06f99a1885d3045d8fd15f94d0bbeb632
Leon Chiu <leontkchiu@gmail.com>
2016-06-03 15:07:41 -0400

    finished rover

--- rover.rb
+++ rover.rb
@@ -1,13 +1,13 @@
 class Rover
 
   def initialize (x_location,y_location,direction)
-    @x_location = x_location
-    @y_location = y_location
-    @direction = direction.upcase
+    @x_location = x_location.to_i
+    @y_location = y_location.to_i
+    @direction = direction.upcase.to_s
   end
 
   def current_position
-    "#{@x_location},#{@y_location},#{@direction}"
+    puts "#{@x_location},#{@y_location},#{@direction}"
   end
 
   def read_instruction (instruction)
@@ -16,7 +16,7 @@ class Rover
         move(x)
       else turn(x)
       end
-      #puts current_position
+      puts current_position
     end
   end
 
@@ -26,7 +26,7 @@ class Rover
     elsif @direction == "E"
       @x_location = @x_location + 1
     elsif @direction == "S"
-      @x_location = @x_location - 1
+      @y_location = @y_location - 1
     else @direction == "W"
       @x_location = @x_location - 1
     end
@@ -57,10 +57,49 @@ class Rover
   end
 
 
-
-#
-
-
 end
+
+
+
+puts "What is the plateau size"
+  plateau_size = gets.chomp
+
+valid_input = false
+  until valid_input == true
+    puts "what is the initial position of the first rover?"
+    initial_position = gets.chomp.split(" ")
+    if initial_position[0].match(/[0-9]/)
+      valid_input = true
+    else
+      valid_input = false
+    end
+  end
+
+
+puts "What are the instructions"
+  a = gets.chomp
+
+  rover1 = Rover.new(initial_position[0], initial_position[1], initial_position[2])
+  rover1.read_instruction(a)
+  rover1.current_position
+
+  valid_input = false
+  until valid_input == true
+    puts "what is the initial position of the second rover?"
+    initial_position = gets.chomp.split(" ")
+    if initial_position[0].match(/[0-9]/)
+      valid_input = true
+    else
+      valid_input = false
+    end
+  end
+
+  puts "What are the instructions"
+    a = gets.chomp
+
+    rover2 = Rover.new(initial_position[0], initial_position[1], initial_position[2])
+    rover2.read_instruction(a)
+    rover2.current_position
+
 
 # leon = Rover.new(1,1,'N')
